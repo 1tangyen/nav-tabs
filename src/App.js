e336e17604c6933d2443785a9831a0254d84ce0b
Yen T <yent@Yens-MacBook-Pro.local>
2023-11-01 14:53:29 -0400

    nav-tabs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Topbar from "./components/Topbar";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Topbar />
+        <Routes>
+          <Route path="/home" element={<h1>Home</h1>} />
+          <Route path="/profile" element={<div>Profile content here</div>} />
+          <Route
+            path="/contact"
+            element={<div>Contact/Status tracker content here</div>}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Topbar.js
@@ -0,0 +1,70 @@
+import { useRef } from "react";
+import { Tabs, Tab } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutline";
+
+import "./Topbar.css";
+
+function Navbar() {
+  const navRef = useRef();
+  const navigate = useNavigate();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
+  return (
+    <header>
+      <h3>LOGO</h3>
+      <nav ref={navRef}>
+        <Tabs
+          defaultActiveKey="/home"
+          id="navbar-tabs"
+          onSelect={(key) => navigate(key)}
+        >
+          <Tab eventKey="/home" title={<>Home</>}>
+            {/* Content for Home tab if needed */}
+          </Tab>
+          <Tab
+            eventKey="/profile"
+            title={
+              <>
+                <PersonOutlinedIcon style={{ marginRight: "5px" }} />
+                My work
+              </>
+            }
+          >
+            {/* Content for My work tab if needed */}
+          </Tab>
+          <Tab
+            eventKey="/contact"
+            title={
+              <>
+                <CalendarTodayOutlinedIcon style={{ marginRight: "5px" }} />
+                Blog
+              </>
+            }
+          >
+            {/* Content for Blog tab if needed */}
+          </Tab>
+          <Tab
+            eventKey="/about"
+            title={
+              <>
+                <HelpOutlineOutlinedIcon style={{ marginRight: "5px" }} />
+                About me
+              </>
+            }
+          >
+            {/* Content for About me tab if needed */}
+          </Tab>
+        </Tabs>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
